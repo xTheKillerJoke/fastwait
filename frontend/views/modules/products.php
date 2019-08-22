17dd06df4297753fe358647ee6011459eda4c998
xTheKillerJoke <xTheKillerJoke@gmail.com>
2019-08-21 22:51:47 -0400

    Products Page

--- frontend/views/modules/products.php
+++ frontend/views/modules/products.php
@@ -384,7 +384,7 @@
 
         $products = ProductsController::ctrShowProducts($order, $item2, $val2, $base, $top);
 
-        var_dump($products["id"]);
+        var_dump($products["title"]);
 
         /* if(!$products)
         {
