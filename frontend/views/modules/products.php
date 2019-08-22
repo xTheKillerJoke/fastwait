9c2b829f5de19f5d3ecb0056b12c83b3f9029d15
xTheKillerJoke <xTheKillerJoke@gmail.com>
2019-08-21 22:42:34 -0400

    Products Page

--- frontend/views/modules/products.php
+++ frontend/views/modules/products.php
@@ -371,18 +371,14 @@
 
           $item2 = "id_subcategory";
           $val2 = $subCategory[0]["id"];
-
-          var_dump($val2);
         }
         else
         {
           $item2 = "id_category";
           $val2 = $category["id"];
-
-          var_dump($val2);
         }
 
-/*         $order = "id";
+        $order = "id";
         $base = 0;
         $top = 20;
 
@@ -452,7 +448,7 @@
                     </div>
                   </div>';
           }
-        } */
+        }
 
       ?>
 
